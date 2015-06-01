e88bcb96cf5eb81acf3e8ff71ff4ac2d140fd40d
freekman <ivan.genchev1989@gmail.com>
2015-06-01 18:01:53 +0300

    Add Configuration property value providfer

---
+++ src/main/java/com/clouway/core/ConfigurationPropertyProvider.java
@@ -0,0 +1,15 @@
+package com.clouway.core;
+
+/**
+ * Created byivan.genchev1989@gmail.com.
+ */
+public class ConfigurationPropertyProvider {
+
+  public String getStringValue(String propertyName) {
+    return null;
+  }
+
+  public String getIntValue(String propertyName) {
+    return null;
+  }
+}
