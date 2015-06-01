8ae3536e6b687a93e9671dc118d53a56b0cf25f1
freekman <ivan.genchev1989@gmail.com>
2015-06-01 11:24:06 +0300

    remove comments in ClientInformation test

--- src/main/test/com/clouway/http/ClientInformationTest.java
+++ src/main/test/com/clouway/http/ClientInformationTest.java
@@ -1,12 +1,20 @@
 package com.clouway.http;
 
+import com.clouway.core.Session;
 import com.clouway.core.SidProvider;
+import com.clouway.core.User;
 import com.clouway.core.UserFinder;
+import com.google.sitebricks.headless.Reply;
+import org.jmock.Expectations;
 import org.jmock.integration.junit4.JUnitRuleMockery;
 import org.junit.Before;
 import org.junit.Rule;
+import org.junit.Test;
 
+import static com.clouway.matshers.ReplyContainsObject.contains;
+import static com.clouway.matshers.ReplyStatus.statusIs;
 import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.MatcherAssert.assertThat;
 
 public class ClientInformationTest {
 
@@ -25,29 +33,29 @@ public class ClientInformationTest {
 
   }
 
-//  @Test
-//  public void deposit() throws Exception {
-//    context.checking(new Expectations() {{
-//      oneOf(sidProvider).get();
-//      will(returnValue("asd"));
-//      oneOf(userFinder).findBySidIfExist("asd");
-//      will(returnValue(new User("Ivan", "qqq", 12.00,new Session("asd","Ivan",0l))));
-//    }});
-//    Reply<?> reply = info.sendInfo();
-//    assertThat(reply, is(contains(new UserDTO("Ivan", "qqq", 12.00))));
-//    assertThat(reply, is(statusIs(200)));
-//  }
-//
-//  @Test
-//  public void sidNotFound() throws Exception {
-//    context.checking(new Expectations() {{
-//      oneOf(sidProvider).get();
-//      will(returnValue(null));
-//      never(userFinder).findBySidIfExist("asd");
-//    }});
-//    Reply<?> reply = info.sendInfo();
-//    assertThat(reply, is(contains("Sid not found!")));
-//    assertThat(reply, is(statusIs(404)));
-//  }
+  @Test
+  public void deposit() throws Exception {
+    context.checking(new Expectations() {{
+      oneOf(sidProvider).get();
+      will(returnValue("asd"));
+      oneOf(userFinder).findBySidIfExist("asd");
+      will(returnValue(new User("Ivan", "qqq", 12.00,new Session("asd","Ivan",0l))));
+    }});
+    Reply<?> reply = info.sendInfo();
+    assertThat(reply, is(contains(new UserDTO("Ivan", "qqq", 12.00))));
+    assertThat(reply, is(statusIs(200)));
+  }
+
+  @Test
+  public void sidNotFound() throws Exception {
+    context.checking(new Expectations() {{
+      oneOf(sidProvider).get();
+      will(returnValue(null));
+      never(userFinder).findBySidIfExist("asd");
+    }});
+    Reply<?> reply = info.sendInfo();
+    assertThat(reply, is(contains("Sid not found!")));
+    assertThat(reply, is(statusIs(404)));
+  }
 
 }
