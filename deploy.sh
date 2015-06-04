5515d784b72f2e55727c6a0408a55fd6bc982579
freekman <ivan.genchev1989@gmail.com>
2015-06-04 13:58:08 +0300

    Add comments to deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,15 +1,13 @@
 #!/bin/bash
 #Deploy to dev.telcong.com the bank
-
 mvn clean install
 #Runn all tests
-
+#If the tests dont pass files wont be deployed
 mvn test
 rc=$?
 if [[ $rc != 0 ]] ; then
  echo 'could not perform tests'; exit $rc
 fi
-
 #Using rsync to copy the files to testDir
 destination=clouway@dev.telcong.com:/opt/telcong/incubator/i-bank/
 rsync target/AngularBank-jar-with-dependencies.jar $destination
@@ -17,4 +15,5 @@ rsync -vr src/main/webapp/ $destination/frontend/
 rsync configuration.properties $destination
 #stop server
 ssh clouway@dev.telcong.com sudo stop upstibank
+#Start server
 ssh clouway@dev.telcong.com sudo start upstibank
